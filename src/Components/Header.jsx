1467716745ef8c9289c6f273844c142846378c0f
Ahednarm <kushwahamahendra691@gmail.com>
2024-09-01 00:38:04 +0530

    Initial commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,11 +4,11 @@ const Header = () => {
   return (
     <>
       <div className="bg-[#10002B] w-full">
-        <div className="flex flex-wrap items-center justify-between
+        <div className=" border border-blue-950 flex flex-wrap items-center justify-between
          text-white py-3 px-12 sm:px-16 md:px-20 lg:px-28">
           {/* Logo */}
           <img src="./public/Logo.png" alt="Logo"
-           className="w-4 md:w-12 lg:w-16 sm:w-8" />
+           className="w-4 md:w-8 lg:w-10 sm:w-6" />
 
           {/* Navigation Links */}
           <div className="flex flex-wrap gap-6 sm:gap-12 md:gap-16 lg:gap-32
